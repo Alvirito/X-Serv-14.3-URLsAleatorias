c644d6ab39b6986be31a4fc65aa9e9e414cc4fb5
alvaro martinez lopez <lopez@lambda38.aulas.gsyc.es>
2015-02-17 11:35:53 +0100

    Terminado

---
+++ servidorAleatorio.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+
+import socket
+import random
+
+# Create a TCP objet socket and bind it to a port
+# Port should be 80, but since it needs root privileges,
+# let's use one above 1024
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Let the port be reused if no process is actually using it
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Bind to the address corresponding to the main name of the host
+mySocket.bind((socket.gethostname(), 1235))
+
+# Queue a maximum of 5 TCP connection requests
+
+mySocket.listen(5)
+
+# Accept connections, read incoming data, and answer back an HTML page
+#  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
+
+try:
+    while True:
+        num_al = random.randrange(99999) 
+        print 'Waiting for connections'
+        (recvSocket, address) = mySocket.accept()
+        print 'Request received:'
+        print recvSocket.recv(2048)
+        print 'Answering back...'
+        recvSocket.send("HTTP/1.1 404 Not found\r\n\r\n" +
+                        "<html><body><h1>¡Hola Mundo!</h1>" +
+                        "<a href='"  + str(num_al)  +"'>¡Dame otra!</a>"  +
+                        "</body></html>" +
+                        "\r\n")
+        recvSocket.close()
+except KeyboardInterrupt:
+    print "Closing binded socket"
+    mySocket.close()
